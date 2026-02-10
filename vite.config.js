5fb8d955083604d4b05945c82f503d49f6e8d584
hansol <ssol7546@gmail.com>
2026-02-10 06:54:55 +0000

    배포 경로 설정 및 조직도 완성

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/postcss'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/my-org-chart/'
+  base: './', // 이 줄을 추가해야 배포 시 파일 경로를 제대로 찾습니다!
 })
